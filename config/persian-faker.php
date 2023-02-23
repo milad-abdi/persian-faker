6ffa0a0ccc3b0da74c8fc7bc5c2babc527ec9f97
milad abdi <milad8742@gmail.com>
2023-02-23 19:06:33 +0330

    Delete unused files.

--- config/persian-faker.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// config for Miladabdi/PersianFaker
-return [
-
-];
--- src/PersianFakerServiceProvider.php
+++ src/PersianFakerServiceProvider.php
@@ -4,22 +4,12 @@ namespace Miladabdi\PersianFaker;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Miladabdi\PersianFaker\Commands\PersianFakerCommand;
 
 class PersianFakerServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
-            ->name('persian-faker')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_persian-faker_table')
-            ->hasCommand(PersianFakerCommand::class);
+            ->name('persian-faker');
     }
 }
